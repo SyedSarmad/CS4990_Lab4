c8c8bef157acb824aa381c401fbebfc78d30181e
Zachbanderson <zachbanderson@gmail.com>
2022-04-22 03:23:14 -0700

    Implementing apriori algorithm

--- patterns.py
+++ patterns.py
@@ -1,5 +1,6 @@
 import math
 import copy
+import collections
 
 # DO NOT CHANGE THE FOLLOWING LINE
 def apriori(itemsets, threshold):
@@ -8,51 +9,71 @@ def apriori(itemsets, threshold):
     # Should return a list of pairs, where each pair consists of the frequent itemset and its support 
     # e.g. [(set(items), 0.7), (set(otheritems), 0.74), ...]
 
+    # We compare everything to itemsets
     kitemsets = []
+    elements = []
+    visited = list()
     for item in itemsets:
-        # print("ITEM: ", end=" ")
-        # print(item, end="")
-        occurrences = itemsets.count(item)
-        # print(" COUNT: ", end=" ")
-        # print(occurrences)
-        if occurrences / len(itemsets) >= threshold:
-            print("ITEM: ", end=" ")
-            print(item, end="")
-            print(" COUNT: ", end=" ")
-            print(occurrences)
-            kitemsets.append(item)
+        for element in item:
+            if element not in visited:
+                visited.append(element)
+                counter = 0
+                for sett in itemsets:
+                    if element in sett:
+                        counter += 1
+
+                if counter/len(itemsets) >= threshold:
+                    kitemsets.append(([element], round(counter/len(itemsets), 2)))
+                    elements.append(element)
+    print(elements)
     print(kitemsets)
-    # removing the duplicates
-    remove_dup(kitemsets)
-    print(kitemsets)
-    two_itemset = []
-    for i in range(len(kitemsets)):
-        temp_list = []
-        temp_list.append(kitemsets[i])
-        #print("temp_list (outer): ", end=" ")
-        #print(temp_list)
-        for val in kitemsets:
-            if kitemsets[i] != val:
-                #print("two_itemset (outer): ", end=" ")
-                #print(two_itemset)
-                #print("val: ", end=" ")
-                #print(val)
-                temp_list.append(val)
-                copy_of_temp = copy.deepcopy(temp_list)
-                #print("temp_list (inner): ",end=" ")
-                #print(temp_list)
-                two_itemset.append(copy_of_temp)
-                #print("two_itemset (inner): ", end=" ")
-                #print(two_itemset)
-                temp_list.pop()
-            else:
-                continue
-    print(two_itemset)
-    # now compare this with the orignal paired one
+
+    visited.clear()
+    frequents = kitemsets.copy()
+
+    # return []
+    while True:
+        tempk = list()
+        print('Lenght of kitemsets is:', len(kitemsets[0][0]))
+        for i in range(len(kitemsets)):
+
+            for j in range(len(kitemsets[0][0]), len(elements)):
+                #print('i is', i)
+                # print('j is', j)
+                joiner = kitemsets[i][0].copy()
+                #print('Before adding, joiner is', joiner)
+                if elements[j] not in joiner:
+                    joiner.append(elements[j])
+                    counter = 0
+                    for sett in itemsets:
+                        if set(joiner.copy()).issubset(sett):
+                            counter += 1
+                    #print('After adding, joiner is', joiner, ' with count', counter)
+                    if counter/len(itemsets) >= threshold:
+                        tempk.append((joiner.copy(), round(counter/len(itemsets), 2)))
+                    #print(tempk)
+                    joiner.clear()
+        if len(tempk) == 0:
+            return removeDupes(kitemsets)
+        else:
+            kitemsets = tempk.copy()
+
+        # print('kitemsets is', kitemsets)
+        # input('Continue')
+
+"""This right here is some ugly-ass code. But I don't care. It's 3:22 AM and it works"""
+def removeDupes(kitemsets):
+    # return kitemsets
+    temp = list()
+    for l in kitemsets:
+        l[0].sort()
+        temp.append((tuple(l[0]), l[1]))
+    # print(temp)
+    # print(set(temp))
+    return set(temp)
 
 
 
-    return []
 
     
 # DO NOT CHANGE THE FOLLOWING LINE
